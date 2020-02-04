88e2222c91bbde7125ff37a47f4a5928e07ac650
Rana Tobia <ranatobia@quickenloans.com>
2020-02-03 21:08:37 -0500

    Lab23

---
+++ CoffeeShopLab23/Controllers/HomeController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CoffeeShopLab23.Models;
+
+namespace CoffeeShopLab23.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //public IActionResult login(string users)
+        //{
+        //    ShopDBContext db = new ShopDBContext();
+
+        //    foreach (Users u in db.Users)
+        //    {
+        //        if (users == u.UserName)
+        //        {
+        //            return View();
+        //        }
+        //       else  return View("Register");
+        //    }
+        //    return View("Shop");
+        //}
+
+        public IActionResult login()
+        {
+            //set a TempData with true when they login
+
+            TempData["username"] = true;
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Shop()
+        {
+            ShopDBContext db = new ShopDBContext();
+
+            //if this is null, they skipped login to view the shop without logging in
+            if (TempData["username"] == null)
+            {
+                TempData["username"] = false;
+            }
+
+            return View(db);
+        }
+        public IActionResult buyItem()
+        {
+           
+            return View();
+        }
+
+      
+
+        public IActionResult MakeNewUser(Users u)
+        {
+            ShopDBContext db = new ShopDBContext();
+
+            db.Users.Add(new Models.Users()
+            {
+                UserName = u.UserName,
+                Email = u.Email,
+                Password = u.Password,
+                Gender = u.Gender,
+                PhoneNumber = u.PhoneNumber,
+                DateOfBirth = u.DateOfBirth,
+                Counrty = u.Counrty,
+            });
+
+            // we should save changes that we just made to our database 
+            db.SaveChanges();
+            return View("Login");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Get(string Items)
+        {
+            var itemObj = new Items();
+            ShopDBContext db = new ShopDBContext();
+            foreach (Items items in db.Items)
+            {
+                itemObj = new Items()
+                {
+                    Id = items.Id,
+                    Name = items.Name,
+                    Description = items.Description,
+                    Price = items.Price,
+                    Quantity = items.Quantity
+
+                };
+            }
+
+
+            return Ok(itemObj);
+        }
+    }
+}
---
+++ CoffeeShopLab23/Models/ShopDBContext.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CoffeeShopLab23.Models
+{
+    public partial class ShopDBContext : DbContext
+    {
+        public ShopDBContext()
+        {
+        }
+
+        public ShopDBContext(DbContextOptions<ShopDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Items> Items { get; set; }
+        public virtual DbSet<Users> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+            optionsBuilder.UseSqlServer("Server=.;Database=ShopDB;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Items>(entity =>
+            {
+                entity.Property(e => e.Description).HasMaxLength(100);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<Users>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Counrty).HasMaxLength(100);
+
+                entity.Property(e => e.DateOfBirth).HasColumnType("date");
+
+                entity.Property(e => e.Email).HasMaxLength(100);
+
+                entity.Property(e => e.Funds).HasColumnName("funds");
+
+                entity.Property(e => e.Gender).HasMaxLength(50);
+
+                entity.Property(e => e.Password).HasMaxLength(50);
+
+                entity.Property(e => e.PhoneNumber).HasMaxLength(50);
+
+                entity.Property(e => e.UserName).HasMaxLength(100);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
